1c35989dc82faa9dd27d2b40c2a7ee412df30938
pudikq@gmail.com <pudikq@gmail.com>
2021-01-15 22:13:18 +0200

    test

--- Pudenko.py
+++ Pudenko.py
@@ -1,1 +1,62 @@
-print("test")
+import random
+import requests
+from paho.mqtt import client as mqtt_client
+
+def telegram_bot_sendtext(bot_message):
+
+    bot_token = '1428837212:AAGr9AnSJOc4MjJTVSynivoPCvDHxyKgj6Y'
+    bot_chatID = '702482339'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+
+#test = telegram_bot_sendtext("Error Ivankov connect")
+
+
+
+
+#count = 0
+broker = 'node.pudenko.com'
+port = 1883
+topic = "Ivankov/LocalTemperature"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+# username = 'emqx'
+# password = 'public'
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        #count = 0
+        #print("count = " + count)
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
+
