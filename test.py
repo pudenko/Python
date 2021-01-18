2f786927724ceb0ab5e1f7079d800ac4a270b1f9
i.pudenko <i.pudenko@etgar.com.ua>
2021-01-18 11:38:47 +0200

    test

--- test.py
+++ test.py
@@ -1,47 +1,49 @@
 import time
 import paho.mqtt.client as paho
-broker="node.pudenko.com"
-reset = 0
+
+broker = "node.pudenko.com"
+resetvalue = 0
 s = 0
 m = 0
 h = 0
 
+def restartTimer():
+    resetvalue = 1
+    print(resetvalue)
+
+
 def on_message(client, userdata, message):
     # time.sleep(1)
-    print("received message =",str(message.payload.decode("utf-8")))
+    print("received message =", str(message.payload.decode("utf-8")))
     if str(message.payload.decode("utf-8")):
-        reset = 1
+        restartTimer()
 
 
-def timer(reset):
- s = 0
- m = 0
- h = 0
- while s<=60:
-    print (h, 'Hours', m, 'Minutes', s, 'Seconds')
-    time.sleep(1)
-    s+=1
-    if s == 60:
-        m+=1
-        s = 0
-    elif m ==60:
-        h+=1
-        m = 0
-        s = 0
-    elif reset > 0:
-        s = 0
-        m = 0
-        h = 0
-        reset = 0
+def timer(resetvalue):
+    s = 0
+    m = 0
+    h = 0
+    while s <= 60:
+        print(h, 'Hours', m, 'Minutes', s, 'Seconds', "     reset = ", resetvalue)
+        time.sleep(1)
+        s += 1
+        if s == 60:
+            m += 1
+            s = 0
+        elif m == 60:
+            h += 1
+            m = 0
+            s = 0
+        elif resetvalue > 0:
+            s = 0
+            m = 0
+            h = 0
+            resetvalue = 0
 
 
-
-client= paho.Client("client-linux-001")
+client = paho.Client("client-linux-001")
 client.on_message = on_message
 client.connect(broker)
 client.loop_start()
 client.subscribe("Ivankov")
-timer(reset)
-
-
-
+timer(resetvalue)
